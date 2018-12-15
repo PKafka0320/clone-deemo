da0fee4efc58ef48cd574ac871e8b9f1d168f5c2
김재훈 <wogns3623@gmail.com>
2018-12-15 12:03:10 +0900

    modify Option Scene

--- forEclipce/rhythm game/src/controller/sceneController/OptionController.java
+++ forEclipce/rhythm game/src/controller/sceneController/OptionController.java
@@ -5,9 +5,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import eventProcesser.SceneChanger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
@@ -19,6 +23,30 @@ public class OptionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
     	Option.setOpacity(0.5);
     	sceneChanger.fadeIn(Option);
+    	VolumeSlide.valueProperty().addListener(new ChangeListener<Object>() {
+			@Override
+			public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) {
+				VolumeValue.textProperty().setValue(
+                        String.valueOf((int) VolumeSlide.getValue()));
+				
+			}
+        });
+    	SyncSlide.valueProperty().addListener(new ChangeListener<Object>() {
+			@Override
+			public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) {
+				SyncValue.textProperty().setValue(
+                        String.valueOf((int) SyncSlide.getValue()));
+				
+			}
+        });
+    	SpeedSlide.valueProperty().addListener(new ChangeListener<Object>() {
+			@Override
+			public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) {
+				SpeedValue.textProperty().setValue(
+                        String.valueOf((int) SpeedSlide.getValue()));
+				
+			}
+        });
     }
 
     @FXML
@@ -26,6 +54,24 @@ public class OptionController implements Initializable {
 
     @FXML
     private Slider VolumeSlide;
+    
+    @FXML
+    private Label VolumeValue;
+    
+    @FXML
+    private Slider SyncSlide;
+    
+    @FXML
+    private Label SyncValue;
+    
+    @FXML
+    private Slider SpeedSlide;
+    
+    @FXML
+    private Label SpeedValue;
+    
+    @FXML
+    private ImageView BacktoMain;
 
     @FXML
 	private void BackToMain(MouseEvent m) throws IOException {
